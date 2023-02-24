d62edf49e6926ba27a87cdb9596340faedb46967
Jonathon <jonnygrealish64@gmail.com>
2023-02-24 18:35:23 +0000

    Organising various files into folders for easier access

---
+++ Topic 1 - HTML Fundamentals/script.js
@@ -0,0 +1,8 @@
+function init() {
+	var h1tags = document.getElementsByTagName("h1");
+	h1tags[1].onclick = react;
+}
+function react() {
+	this.innerHTML = "Clicked!"; this.style.color = "red";
+}
+onload = init;
